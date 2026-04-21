8394ec4a8c8a7ab493040cd63c9b3393af4ae2b6
konst-an <konst.anastasia02@gmail.com>
2026-04-21 16:17:46 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -17,27 +17,27 @@ module.exports = {
         port: 3000,
     },
 
-  module: {
-    rules: [
-    {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-    },
-    {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-    },
-    {
-        test: /\.(png|jpg|gif)$/i,
-        type: 'asset/resource',
-    },
-    ],
-  },
+      module: {
+        rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: 'babel-loader',
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|jpg|gif)$/i,
+            type: 'asset/resource',
+        },
+        ],
+      },
 
   plugins: [
-    new HtmlWebpackPlugin({
-    template: './src/index.html',
+        new HtmlWebpackPlugin({
+        template: './src/index.html',
     }),
   ],
 };
